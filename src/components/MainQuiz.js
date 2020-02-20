71526e60e5d573ec283292ae9a095a3ea80f0c41
Piotr Talaga <piotrek.talaga@gmail.com>
2020-02-21 00:20:20 +0100

    quiz funcionality done

--- src/components/MainQuiz.js
+++ src/components/MainQuiz.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import data from '../questions';
+import Question from "./Question";
 
+let questions = data.questions;
 
 class MainQuiz extends Component {
     constructor(props) {
@@ -8,13 +10,41 @@ class MainQuiz extends Component {
         this.state = {
             economicScore: 0,
             socialScore: 0,
-            current: 1
+            currentQuestion: 1
         }
     }
+
+    // funkcja, zmieniająca uzyskany wynik, w zależności od odpowiedzi. Pierwszy argument określa, czy zmieniamy wynik dla poglądów
+    // ekonomicznych, bądź społecznych. Drugi określa czy w zależności od odpowiedzi wynik ma rosnąć bądź maleć, a trzeci odpowiada wartości udzielonej odpowiedzi.
+    setScore = (typeOfQuestion, orientation, answerValue) => {
+        let scoreAddition = orientation * answerValue;
+        if (typeOfQuestion === 'economic') {
+            this.setState({
+                economicScore: this.state.economicScore + scoreAddition
+            });
+        }
+        else if (typeOfQuestion === 'social') {
+            this.setState({
+                socialScore: this.state.socialScore + scoreAddition
+            });
+        }
+    };
+
+    setCurrentQuestion = (id) =>{
+        this.setState({
+            currentQuestion: id
+        });
+    };
     render() {
         return (
             <section className='main quiz'>
-                <h1>Tu będzie ten quiz</h1>
+                {questions.map(question => {
+                    if (this.state.currentQuestion === question.id) {
+                        return <Question question={question} currentQuestion={this.state.currentQuestion} setScore={this.setScore}
+                                         setCurrentQuestion={this.setCurrentQuestion}/>
+                    }
+                })}
+                <p>wynik ekonomiczny: {this.state.economicScore}, wynik społeczny: {this.state.socialScore}</p>
             </section>
         )
     }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,64 @@
 import React, {Component} from 'react';
 
 class Question extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedValue: null
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            selectedValue: e.target.value
+        });
+    };
+
+    handleSubmit = (e, selectedValue) => {
+        e.preventDefault();
+        let {question, currentQuestion, setScore, setCurrentQuestion} = this.props;
+        setScore(question.typeOfQuestion, question.orientation, this.state.selectedValue);
+        this.setState({
+            selectedValue: null
+        });
+        setCurrentQuestion(currentQuestion+1);
+    };
 
     render() {
+        let {selectedValue} = this.state;
+        let {question} = this.props;
         return (
-            <section>No, będą pytanka, spokojnie ;)</section>
+            <section>
+                <h2> {question.question}</h2>
+                <form onSubmit={e => this.handleSubmit(e, selectedValue)}>
+                    <label className='answers'>
+                        <input type='radio'name='answers' value='2' onChange={this.handleChange}/>
+                        <span className="answersCheckmark"></span>
+                        Zupełnie się zgadzam
+                    </label>
+                    <label className='answers'>
+                        <input type='radio'name='answers' value='1' onChange={this.handleChange}/>
+                        <span className="answersCheckmark"></span>
+                        Raczej się zgadzam
+                    </label>
+                    <label className='answers'>
+                        <input type='radio'name='answers' value='0' onChange={this.handleChange}/>
+                        <span className="answersCheckmark"></span>
+                        Nie mam zdania
+                    </label>
+                    <label className='answers'>
+                        <input type='radio'name='answers' value='-1' onChange={this.handleChange}/>
+                        <span className="answersCheckmark"></span>
+                        Raczej się nie zgadzam
+                    </label>
+                    <label className='answers'>
+                        <input type='radio'name='answers' value='-2' onChange={this.handleChange}/>
+                        <span className="answersCheckmark"></span>
+                        Zupełnie się nie zgadzam
+                    </label>
+                    <button disabled={selectedValue===null}>Dalej</button>
+                </form>
+            </section>
         )
     }
 }
